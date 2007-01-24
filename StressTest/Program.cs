1003badf29be56a8b879289fa54620719102aaf5
n.takushima <n.takushima@868e30aa-a527-0410-aa16-f5559cb7211e>
2007-01-24 18:44:44 +0000

    ConsoleServerと同じものが同じコードで実装できた

--- StressTest/Program.cs
+++ StressTest/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using RakNetDotNet;
 
 namespace StressTest
 {
@@ -8,6 +9,35 @@ namespace StressTest
     {
         static void Main(string[] args)
         {
+            TelnetTransport tt = new TelnetTransport();
+            TestCommandServer(tt, 23);
+        }
+
+        static void TestCommandServer(TransportInterface ti, ushort port)
+        {
+            ConsoleServer consoleServer = new ConsoleServer();
+            RakNetCommandParser rcp = new RakNetCommandParser();
+            LogCommandParser lcp = new LogCommandParser();
+            uint lastlog = 0;
+            RakPeerInterface rakPeer = RakNetworkFactory.GetRakPeerInterface();
+
+            consoleServer.AddCommandParser(rcp);
+            consoleServer.AddCommandParser(lcp);
+            consoleServer.SetTransportProvider(ti, port);
+            rcp.SetRakPeerInterface(rakPeer);
+            lcp.AddChannel("TestChannel");
+            while (true)
+            {
+                consoleServer.Update();
+
+                if (RakNetDotNet.RakNet.GetTime() > lastlog + 4000)
+                {
+                    lcp.WriteLog("TestChannel", "Test of logger");
+                    lastlog = RakNetDotNet.RakNet.GetTime();
+                }
+
+                System.Threading.Thread.Sleep(30);
+            }
         }
     }
 }
