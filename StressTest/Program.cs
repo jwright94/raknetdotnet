45cf456d27bb6ac2089d01c493cf64207c04532f
n.takushima <n.takushima@868e30aa-a527-0410-aa16-f5559cb7211e>
2007-01-26 01:24:36 +0000

    実装中だけどあげ

--- StressTest/Program.cs
+++ StressTest/Program.cs
@@ -9,17 +9,62 @@ namespace StressTest
     {
         static void Main(string[] args)
         {
-            TelnetTransport tt = new TelnetTransport();
-            TestCommandServer(tt, 23);
+            char ch;
+            bool isServer;
+            RakPeerInterface serverRakPeer = RakNetworkFactory.GetRakPeerInterface();
+            List<RakPeerInterface> clientRakPeers = new List<RakPeerInterface>();
+            int clustorSize = 16;
+            int maxNumberOfClustors = 1024/clustorSize;
+            Console.WriteLine("(S)erver or (C)lient?");
+            ch = Console.ReadKey(true).KeyChar;
+            if (ch == 's')
+            {
+                isServer = true;
+                serverRakPeer.SetMaximumIncomingConnections(1024);
+                SocketDescriptor socketDescriptor = new SocketDescriptor(12345, string.Empty);
+                serverRakPeer.Startup(1024, 0, new SocketDescriptor[] { socketDescriptor }, 1);
+                Console.WriteLine("Server started");
+            }
+            else
+            {
+                isServer = false;
+                Console.Write("Number of Clients: ");
+                string numberOfClientsString = Console.ReadLine();
+                int numberOfClients = Convert.ToInt32(numberOfClientsString);
+                SocketDescriptor socketDescriptor = new SocketDescriptor();
+                for (int i = 0; i < clustorSize * numberOfClients; ++i)
+                {
+                    RakPeerInterface clientRakPeer = RakNetworkFactory.GetRakPeerInterface();
+                    clientRakPeer.Startup(1, 0, new SocketDescriptor[] { socketDescriptor }, 1);
+                    clientRakPeers.Add(clientRakPeer);
+                }
+                Console.WriteLine("Client started");
+                Console.Write("Enter server IP: ");
+                string serverIP = Console.ReadLine();
+                Console.WriteLine("Connecting to server.");
+                foreach (RakPeerInterface clientRakPeer in clientRakPeers)
+                {
+                    clientRakPeer.Connect(serverIP, 12345, string.Empty, 0);
+                }
+            }
+            Console.WriteLine("(E)xit");
+            while (true)
+            {
+                
+                System.Threading.Thread.Sleep(0);
+            }
+
+            //TelnetTransport tt = new TelnetTransport();
+            //RakPeerInterface rakPeer = RakNetworkFactory.GetRakPeerInterface();
+            //TestCommandServer(tt, 23, rakPeer);
         }
 
-        static void TestCommandServer(TransportInterface ti, ushort port)
+        static void TestCommandServer(TransportInterface ti, ushort port, RakPeerInterface rakPeer)
         {
             ConsoleServer consoleServer = new ConsoleServer();
             RakNetCommandParser rcp = new RakNetCommandParser();
             LogCommandParser lcp = new LogCommandParser();
             uint lastlog = 0;
-            RakPeerInterface rakPeer = RakNetworkFactory.GetRakPeerInterface();
             IntPtr testChannel = System.Runtime.InteropServices.Marshal.StringToHGlobalAnsi("TestChannel");  // you must call FreeHGlobal
 
             consoleServer.AddCommandParser(rcp);
