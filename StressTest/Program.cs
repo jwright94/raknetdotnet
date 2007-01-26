f0a0140065edfafb9de00c3c582c2bd62c8334dc
n.takushima <n.takushima@868e30aa-a527-0410-aa16-f5559cb7211e>
2007-01-26 05:10:29 +0000

    echoするだけのテストができた

--- StressTest/Program.cs
+++ StressTest/Program.cs
@@ -5,6 +5,17 @@ using RakNetDotNet;
 
 namespace StressTest
 {
+    class Connection
+    {
+        public RakPeerInterface RakPeer;
+        public bool ConnectionCompleted;
+        public Connection(RakPeerInterface rakPeer, bool connectionCompleted)
+        {
+            RakPeer = rakPeer;
+            ConnectionCompleted = connectionCompleted;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -12,17 +23,23 @@ namespace StressTest
             char ch;
             bool isServer;
             RakPeerInterface serverRakPeer = RakNetworkFactory.GetRakPeerInterface();
-            List<RakPeerInterface> clientRakPeers = new List<RakPeerInterface>();
-            int clustorSize = 16;
-            int maxNumberOfClustors = 1024/clustorSize;
+            const int maxNumberOfClients = 2048;
+            IList<Connection> clientConnections = new List<Connection>(maxNumberOfClients);
+            const ushort portNum = 12345;
+            int querySize = 1000;
+            byte[] dummyData = new byte[querySize];
+            Random r = new Random();
+            r.NextBytes(dummyData);
+            dummyData[0] = (byte)RakNetDotNet.RakNet.ID_USER_PACKET_ENUM;
+            List<SystemAddress> incomingConnections = new List<SystemAddress>(maxNumberOfClients);
             Console.WriteLine("(S)erver or (C)lient?");
             ch = Console.ReadKey(true).KeyChar;
             if (ch == 's')
             {
                 isServer = true;
-                serverRakPeer.SetMaximumIncomingConnections(1024);
-                SocketDescriptor socketDescriptor = new SocketDescriptor(12345, string.Empty);
-                serverRakPeer.Startup(1024, 0, new SocketDescriptor[] { socketDescriptor }, 1);
+                serverRakPeer.SetMaximumIncomingConnections(maxNumberOfClients);
+                SocketDescriptor socketDescriptor = new SocketDescriptor(portNum, string.Empty);
+                serverRakPeer.Startup(maxNumberOfClients, 0, new SocketDescriptor[] { socketDescriptor }, 1);
                 Console.WriteLine("Server started");
             }
             else
@@ -31,32 +48,99 @@ namespace StressTest
                 Console.Write("Number of Clients: ");
                 string numberOfClientsString = Console.ReadLine();
                 int numberOfClients = Convert.ToInt32(numberOfClientsString);
+                Console.Write("Query size: ");
+                string querySizeString = Console.ReadLine();
+                querySize = Convert.ToInt32(querySizeString);
                 SocketDescriptor socketDescriptor = new SocketDescriptor();
-                for (int i = 0; i < clustorSize * numberOfClients; ++i)
+                for (int i = 0; i < numberOfClients; ++i)
                 {
                     RakPeerInterface clientRakPeer = RakNetworkFactory.GetRakPeerInterface();
                     clientRakPeer.Startup(1, 0, new SocketDescriptor[] { socketDescriptor }, 1);
-                    clientRakPeers.Add(clientRakPeer);
+                    clientConnections.Add(new Connection(clientRakPeer, false));
                 }
                 Console.WriteLine("Client started");
                 Console.Write("Enter server IP: ");
                 string serverIP = Console.ReadLine();
                 Console.WriteLine("Connecting to server.");
-                foreach (RakPeerInterface clientRakPeer in clientRakPeers)
+                foreach (Connection clientConnection in clientConnections)
                 {
-                    clientRakPeer.Connect(serverIP, 12345, string.Empty, 0);
+                    clientConnection.RakPeer.Connect(serverIP, portNum, string.Empty, 0);
                 }
             }
-            Console.WriteLine("(E)xit");
+            //Console.WriteLine("(E)xit");
+            uint lastLog = 0;
             while (true)
             {
-                
+                if (isServer)
+                {
+                    Packet p = serverRakPeer.Receive();
+                    if (p != null)
+                    {
+                        byte packetIdentifier = GetPacketIdentifier(p);
+                        if (packetIdentifier == RakNetDotNet.RakNet.ID_NEW_INCOMING_CONNECTION)
+                        {
+                            Console.WriteLine("ID_NEW_INCOMING_CONNECTION");
+                            //incomingConnections.Add(p.systemAddress);
+                        }
+                        else if (packetIdentifier == RakNetDotNet.RakNet.ID_USER_PACKET_ENUM)
+                        {
+                            serverRakPeer.Send(p.data, (int)p.length, PacketPriority.HIGH_PRIORITY, PacketReliability.RELIABLE_ORDERED, 0, p.systemAddress, false);
+                        }
+                        serverRakPeer.DeallocatePacket(p);
+                    }
+
+                    SystemAddress systemAddress = serverRakPeer.GetSystemAddressFromIndex(0);
+                    //if (systemAddress.binaryAddress != RakNetDotNet.RakNet.UNASSIGNED_SYSTEM_ADDRESS.binaryAddress &&
+                    //    systemAddress.port != RakNetDotNet.RakNet.UNASSIGNED_SYSTEM_ADDRESS.port)
+                    if(!systemAddress.Equals(RakNetDotNet.RakNet.UNASSIGNED_SYSTEM_ADDRESS))
+                    {
+                        RakNetStatisticsStruct rss = serverRakPeer.GetStatistics(systemAddress);
+                        if (RakNetDotNet.RakNet.GetTime() > lastLog + 4000)
+                        {
+                            Console.WriteLine("Packets sent:\t\t\t\t{0:D}", rss.packetsSent);
+                            lastLog = RakNetDotNet.RakNet.GetTime();
+                        }
+                    }
+                }
+                else
+                {
+                    foreach(Connection clientConnection in clientConnections)
+                    {
+                        Packet p = clientConnection.RakPeer.Receive();
+                        if (p != null)
+                        {
+                            byte packetIdentifier = GetPacketIdentifier(p);
+                            if (packetIdentifier == RakNetDotNet.RakNet.ID_CONNECTION_REQUEST_ACCEPTED)
+                            {
+                                Console.WriteLine("ID_CONNECTION_REQUEST_ACCEPTED");
+                                clientConnection.ConnectionCompleted = true;
+                            }
+                            clientConnection.RakPeer.DeallocatePacket(p);
+                        }
+
+                        if (clientConnection.ConnectionCompleted)
+                        {
+                            clientConnection.RakPeer.Send(dummyData, querySize, PacketPriority.HIGH_PRIORITY, PacketReliability.RELIABLE_ORDERED, 0, RakNetDotNet.RakNet.UNASSIGNED_SYSTEM_ADDRESS, true);
+                        }
+                    }
+                }
                 System.Threading.Thread.Sleep(0);
             }
+
+            //foreach (RakPeerInterface clientRakPeer in clientRakPeers)
+            //{
+            //    RakNetworkFactory.DestroyRakPeerInterface(clientRakPeer);
+            //}
+            //RakNetworkFactory.DestroyRakPeerInterface(serverRakPeer);
 
             //TelnetTransport tt = new TelnetTransport();
             //RakPeerInterface rakPeer = RakNetworkFactory.GetRakPeerInterface();
             //TestCommandServer(tt, 23, rakPeer);
+        }
+
+        static byte GetPacketIdentifier(Packet p)
+        {
+            return p.data[0];
         }
 
         static void TestCommandServer(TransportInterface ti, ushort port, RakPeerInterface rakPeer)
