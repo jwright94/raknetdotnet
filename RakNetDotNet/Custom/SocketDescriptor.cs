eb0853f44ed6661ef03870e5b92a75a1f673dcdd
n.takushima <n.takushima@868e30aa-a527-0410-aa16-f5559cb7211e>
2007-01-31 14:03:54 +0000

    Replicaを継承可能にしようとしてやめた

--- RakNetDotNet/Custom/SocketDescriptor.cs
+++ RakNetDotNet/Custom/SocketDescriptor.cs
@@ -1,18 +1,21 @@
-namespace RakNetDotNet {
+namespace RakNetDotNet
+{
 
-using System;
-using System.Runtime.InteropServices;
+    using System;
+    using System.Runtime.InteropServices;
 
-[StructLayout(LayoutKind.Sequential, CharSet=CharSet.Ansi)]
-public struct SocketDescriptor {
-  public SocketDescriptor(ushort _port, string _hostAddress) {
-      port = _port;
-      hostAddress = _hostAddress;
-  }
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+    public struct SocketDescriptor
+    {
+        public SocketDescriptor(ushort _port, string _hostAddress)
+        {
+            port = _port;
+            hostAddress = _hostAddress;
+        }
 
-  public ushort port;
-  [MarshalAs(UnmanagedType.LPStr, SizeConst=32)]
-  public string hostAddress;
-}
+        public ushort port;
+        [MarshalAs(UnmanagedType.LPStr, SizeConst = 32)]
+        public string hostAddress;
+    }
 
 }
