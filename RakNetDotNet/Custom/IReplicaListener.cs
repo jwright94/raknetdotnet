9d41828c2b78271fd26998bf7b1efa3768f13e71
n.takushima <n.takushima@868e30aa-a527-0410-aa16-f5559cb7211e>
2007-02-01 07:36:13 +0000

    ReplicaMember実装完了?

---
+++ RakNetDotNet/Custom/IReplicaListener.cs
@@ -0,0 +1,33 @@
+﻿namespace RakNetDotNet
+{
+    public interface IReplicaListener
+    {
+        ReplicaReturnResult Deserialize(BitStream inBitStream, uint timestamp, uint lastDeserializeTime, SystemAddress systemAddress);
+        ReplicaReturnResult ReceiveDestruction(BitStream inBitStream, SystemAddress systemAddress, uint timestamp);
+        ReplicaReturnResult ReceiveScopeChange(BitStream inBitStream, SystemAddress systemAddress, uint timestamp);
+        ReplicaReturnResult SendConstruction(uint currentTime, SystemAddress systemAddress, BitStream outBitStream, ref bool includeTimestamp);
+        void SendDestruction(BitStream outBitStream, SystemAddress systemAddress, ref bool includeTimestamp);
+        ReplicaReturnResult SendScopeChange(bool inScope, BitStream outBitStream, uint currentTime, SystemAddress systemAddress, ref bool includeTimestamp);
+        ReplicaReturnResult Serialize(SWIGTYPE_p_bool sendTimestamp, BitStream outBitStream, uint lastSendTime, ref PacketPriority priority, ref PacketReliability reliability, uint currentTime, SystemAddress systemAddress, SWIGTYPE_p_unsigned_int flags);
+    }
+
+    public sealed class NullReplicaListener : IReplicaListener
+    {
+        private static readonly NullReplicaListener instance = new NullReplicaListener();
+        private NullReplicaListener() { }
+        public static NullReplicaListener Instance
+        {
+            get { return instance; }
+        }
+
+        #region IReplicaListener Members
+        public ReplicaReturnResult Deserialize(BitStream inBitStream, uint timestamp, uint lastDeserializeTime, SystemAddress systemAddress) { return ReplicaReturnResult.REPLICA_CANCEL_PROCESS; }
+        public ReplicaReturnResult ReceiveDestruction(BitStream inBitStream, SystemAddress systemAddress, uint timestamp) { return ReplicaReturnResult.REPLICA_CANCEL_PROCESS; }
+        public ReplicaReturnResult ReceiveScopeChange(BitStream inBitStream, SystemAddress systemAddress, uint timestamp) { return ReplicaReturnResult.REPLICA_CANCEL_PROCESS; }
+        public ReplicaReturnResult SendConstruction(uint currentTime, SystemAddress systemAddress, BitStream outBitStream, ref bool includeTimestamp) { return ReplicaReturnResult.REPLICA_CANCEL_PROCESS; }
+        public void SendDestruction(BitStream outBitStream, SystemAddress systemAddress, ref bool includeTimestamp) { }
+        public ReplicaReturnResult SendScopeChange(bool inScope, BitStream outBitStream, uint currentTime, SystemAddress systemAddress, ref bool includeTimestamp) { return ReplicaReturnResult.REPLICA_CANCEL_PROCESS; }
+        public ReplicaReturnResult Serialize(SWIGTYPE_p_bool sendTimestamp, BitStream outBitStream, uint lastSendTime, ref PacketPriority priority, ref PacketReliability reliability, uint currentTime, SystemAddress systemAddress, SWIGTYPE_p_unsigned_int flags) { return ReplicaReturnResult.REPLICA_CANCEL_PROCESS; }
+        #endregion
+    }
+}
--- RakNetDotNet/Custom/ReplicaMember.cs
+++ RakNetDotNet/Custom/ReplicaMember.cs
@@ -2,5 +2,49 @@ namespace RakNetDotNet
 {
     public class ReplicaMember : Replica
     {
+        private IReplicaListener listener = NullReplicaListener.Instance;
+        public IReplicaListener Listener
+        {
+            get { return listener; }
+            set { listener = value; }
+        }
+
+        public ReplicaMember() { }
+        
+        public ReplicaMember(IReplicaListener listener) 
+        {
+            this.listener = listener;
+        }
+
+        #region Replica Members
+        public override ReplicaReturnResult Deserialize(BitStream inBitStream, uint timestamp, uint lastDeserializeTime, SystemAddress systemAddress) 
+        { 
+            return listener.Deserialize(inBitStream, timestamp, lastDeserializeTime, systemAddress); 
+        }
+        public override ReplicaReturnResult ReceiveDestruction(BitStream inBitStream, SystemAddress systemAddress, uint timestamp)
+        {
+            return listener.ReceiveDestruction(inBitStream, systemAddress, timestamp);
+        }
+        public override ReplicaReturnResult ReceiveScopeChange(BitStream inBitStream, SystemAddress systemAddress, uint timestamp)
+        {
+            return listener.ReceiveScopeChange(inBitStream, systemAddress, timestamp);
+        }
+        public override ReplicaReturnResult SendConstruction(uint currentTime, SystemAddress systemAddress, BitStream outBitStream, ref bool includeTimestamp)
+        {
+            return listener.SendConstruction(currentTime, systemAddress, outBitStream, ref includeTimestamp);
+        }
+        public override void SendDestruction(BitStream outBitStream, SystemAddress systemAddress, ref bool includeTimestamp)
+        {
+            listener.SendDestruction(outBitStream, systemAddress, ref includeTimestamp);
+        }
+        public override ReplicaReturnResult SendScopeChange(bool inScope, BitStream outBitStream, uint currentTime, SystemAddress systemAddress, ref bool includeTimestamp)
+        {
+            return listener.SendScopeChange(inScope, outBitStream, currentTime, systemAddress, ref includeTimestamp);
+        }
+        public override ReplicaReturnResult Serialize(SWIGTYPE_p_bool sendTimestamp, BitStream outBitStream, uint lastSendTime, ref PacketPriority priority, ref PacketReliability reliability, uint currentTime, SystemAddress systemAddress, SWIGTYPE_p_unsigned_int flags)
+        {
+            return listener.Serialize(sendTimestamp, outBitStream, lastSendTime, ref priority, ref reliability, currentTime, systemAddress, flags);
+        }
+        #endregion
     }
 }
