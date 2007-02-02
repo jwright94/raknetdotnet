39a2ca4153464370fcb86f310cad79b14d9dd089
n.takushima <n.takushima@868e30aa-a527-0410-aa16-f5559cb7211e>
2007-02-02 14:35:06 +0000

    サンプルちゃんと実装中

--- Samples/CommandConsoleServer/Program.cs
+++ Samples/CommandConsoleServer/Program.cs
@@ -24,7 +24,14 @@ namespace CommandConsoleServer
             RakPeerInterface rakPeer = RakNetworkFactory.GetRakPeerInterface();
             IntPtr testChannel = System.Runtime.InteropServices.Marshal.StringToHGlobalAnsi("TestChannel");  // you must call FreeHGlobal
 
-            Console.WriteLine("Command server started on port {0}.", port);
+            Console.Write("This sample demonstrates the command console server, which can be.\n");
+            Console.Write("a standalone application or part of your game server.  It allows you to\n");
+            Console.Write("easily parse text strings sent from a client using either secure RakNet\n");
+            Console.Write("or Telnet.\n");
+            Console.Write("See the 'CommandConsoleClient' project for the RakNet client.\n");
+            Console.Write("Difficulty: Intermediate\n\n");
+
+            Console.Write("Command server started on port {0}.\n", port);
             consoleServer.AddCommandParser(rcp);
             consoleServer.AddCommandParser(lcp);
             consoleServer.SetTransportProvider(ti, port);
--- Samples/ObjectMemberRPC/Program.cs
+++ Samples/ObjectMemberRPC/Program.cs
@@ -8,16 +8,57 @@ namespace ObjectMemberRPC
 
     class Apple
     {
-        void func1(RPCParameters rpcParams)
+        public Apple()
+        {
+            gen.Parent = this;
+        }
+
+        public virtual void func1(RPCParameters rpcParams)
         {
             byte[] input = rpcParams.input;
             if (0 < input.Length)
-            {
-                //Console.WriteLine("Base Apple func1: {0}", UnicodeEncoding.
-            }
+                Console.Write("Base Apple func1: {0}\n", GetString(input));
             else
-            {
-            }
+                Console.Write("Base Apple func1\n");
+        }
+
+        public virtual void func1(string blah)
+        {
+            Console.Write("Func1.  Does not match function signature and should never be called.\n");
+        }
+
+        public virtual void func2(RPCParameters rpcParams)
+        {
+            byte[] input = rpcParams.input;
+            if(0 < input.Length)
+                Console.Write("Base Apple func2: {0}\n", GetString(input));
+            else
+                Console.Write("Base Apple func2\n");
+        }
+
+        public virtual void func3(RPCParameters rpcParams)
+        {
+            byte[] input = rpcParams.input;
+            if(0 < input.Length)
+                Console.Write("Base Apple func3: {0}\n", GetString(input));
+            else
+                Console.Write("Base Apple func3\n");
+        }
+
+        protected string GetString(byte[] input)
+        {
+            Encoding encoder = new UnicodeEncoding();
+            return encoder.GetString(input);
+        }
+
+        private ServerNetworkIDGenerator gen = new ServerNetworkIDGenerator();
+    }
+
+    class GrannySmith : Apple
+    {
+        public override void func1(RPCParameters rpcParams)
+        {
+            Console.Write("Derived GrannySmith func1: {0}\n", GetString(rpcParams.input));
         }
     }
 
@@ -25,6 +66,37 @@ namespace ObjectMemberRPC
     {
         static void Main(string[] args)
         {
+            uint time;
+            RakPeerInterface rakPeer1 = RakNetworkFactory.GetRakPeerInterface();
+            RakPeerInterface rakPeer2 = RakNetworkFactory.GetRakPeerInterface();
+
+            Console.Write("This project shows how to call member functions of your application object.\n");
+            Console.Write("instances.\n");
+            Console.Write("Difficulty: Advanced\n\n");
+
+            SocketDescriptor socketDescriptor = new SocketDescriptor(60000, string.Empty);
+            rakPeer1.Startup(2, 0, new SocketDescriptor[] { socketDescriptor }, 1);
+            socketDescriptor.port = 60001;
+            rakPeer2.Startup(2, 0, new SocketDescriptor[] { socketDescriptor }, 1);
+            rakPeer2.SetMaximumIncomingConnections(2);
+            rakPeer1.Connect("127.0.0.1", 60001, string.Empty, 0);
+
+            rakPeer2.RegisterClassMemberRPC(typeof(Apple).GetMethod("func1"));
+            rakPeer2.RegisterClassMemberRPC(typeof(Apple).GetMethod("func2"));
+            rakPeer2.RegisterClassMemberRPC(typeof(Apple).GetMethod("func3"));
+
+            System.Threading.Thread.Sleep(250);
+
+            Apple apple = new GrannySmith();
+            time = RakNetBindings.GetTime();
+
+            //Console.Write("Calling func1 of Apple base class with test string 1.\n");
+            //rakPeer1.RPC(typeof(Apple).GetMethod("func1"), "test string 1", (int)(strlen("test string 1") + 1) * 8, HIGH_PRIORITY, RELIABLE_ORDERED, 0, UNASSIGNED_SYSTEM_ADDRESS, true, 0, apple->GetNetworkID(), 0);
+            //Console.Write("Calling func2 of Apple base class with test string 2.\n");
+            //rakPeer1.RPC(CLASS_MEMBER_ID(Apple, func2), "test string 2", (int)(strlen("test string 2") + 1) * 8, HIGH_PRIORITY, RELIABLE_ORDERED, 0, UNASSIGNED_SYSTEM_ADDRESS, true, 0, apple->GetNetworkID(), 0);
+            //Console.Write("Calling func3 of Apple base class with no test string.\n");
+            //rakPeer1.RPC(CLASS_MEMBER_ID(Apple, func3), 0, 0, HIGH_PRIORITY, RELIABLE_ORDERED, 0, UNASSIGNED_SYSTEM_ADDRESS, true, 0, apple->GetNetworkID(), 0);
+
         }
     }
 }
