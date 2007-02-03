f5ca56b19706088cbbf42e7916246da8f2b627d4
n.takushima <n.takushima@868e30aa-a527-0410-aa16-f5559cb7211e>
2007-02-03 16:47:06 +0000

    BitStream が RPCParameters を受け取れるようにした。

--- Samples/BitStream/Program.cs
+++ Samples/BitStream/Program.cs
@@ -4,10 +4,29 @@ using System.Text;
 
 namespace BitStream
 {
+    using RakNetDotNet;
+
     class Program
     {
+        static bool quit;
+
+        struct EmploymentStruct
+        {
+            int salary;
+            byte yearsEmployed;
+        }
+
+        static void clientRPC(RPCParameters rpcParameters)
+        {
+            BitStream b = new BitStream(rpcParameters, false);
+        }
+
         static void Main(string[] args)
         {
+            RakPeerInterface rakClient = RakNetworkFactory.GetRakPeerInterface();
+	        RakPeerInterface rakServer = RakNetworkFactory.GetRakPeerInterface();
+
+
         }
     }
 }
