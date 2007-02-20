962fddfc05c92afbc3b8113461e58260249e3747
n.takushima <n.takushima@868e30aa-a527-0410-aa16-f5559cb7211e>
2007-02-20 18:45:23 +0000

    ちょっとしたイベントシステムを書いてみる

---
+++ Samples/EventSystem/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
