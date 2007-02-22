81d7c76ea062bff7f8ac577fd0c96f61c823728e
n.takushima <n.takushima@868e30aa-a527-0410-aa16-f5559cb7211e>
2007-02-22 05:39:20 +0000

    EventCenterClient書いてみた

--- Samples/EventSystem/Program.cs
+++ Samples/EventSystem/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 namespace EventSystem
 {
     using RakNetDotNet;
+    using System.Diagnostics;
 
     interface IEvent
     {
@@ -71,14 +72,27 @@ namespace EventSystem
         public NetworkException(string error) : base(error) { }
     }
 
-    sealed class RpcCalls
+    sealed class RpcCalls : IDisposable
     {
-        #region Implementing the singleton pattern.
-        static readonly RpcCalls instance = new RpcCalls();
-        RpcCalls() { }
+        #region Ogre-like singleton implementation.
+        static RpcCalls instance;
+        public RpcCalls() 
+        {  
+            Debug.Assert(instance == null);
+            instance = this;
+        }
+        public void  Dispose()
+        {
+            Debug.Assert(instance != null);
+ 	        instance = null;
+        }
         public static RpcCalls Instance
         {
-            get { return instance; }
+            get 
+            { 
+                Debug.Assert(instance != null);
+                return instance; 
+            }
         }
         #endregion
         static void SendEventToClient(RPCParameters _params)
@@ -107,13 +121,195 @@ namespace EventSystem
 
     sealed class EventCenterClient
     {
-        #region Implementing the singleton pattern.
-        static readonly EventCenterClient instance = new EventCenterClient();
-        EventCenterClient() { }
+        #region Ogre-like singleton implementation.
+        static EventCenterClient instance;
+        public EventCenterClient(string xmlFile) 
+        {  
+            Debug.Assert(instance == null);
+            instance = this;
+
+            Load(xmlFile);
+            isConnected = false;
+
+            if (isOnline)
+            {
+                rakClientInterface = RakNetworkFactory.GetRakPeerInterface();
+                rakClientInterface.RegisterAsRemoteProcedureCall("sendeventtoclient", typeof(RpcCalls).GetMethod("SendEventToClient"));
+            }
+        }
+        public void  Dispose()
+        {
+            Debug.Assert(instance != null);
+ 	        instance = null;
+
+            if (IsOnline)
+            {
+                log("Closing connection...");
+                rakClientInterface.Shutdown(10);
+                rakClientInterface.UnregisterAsRemoteProcedureCall("sendeventtoclient");
+                RakNetworkFactory.DestroyRakPeerInterface(rakClientInterface);
+                log("Closed.");
+            }
+        }
         public static EventCenterClient Instance
         {
-            get { return instance; }
+            get 
+            { 
+                Debug.Assert(instance != null);
+                return instance; 
+            }
         }
+        #endregion
+        public void OverrideClientPort(ushort newPort)
+        {
+            clientPort = newPort;
+            log("Overwrote client port = {0}", clientPort);
+        }
+        public void OverrideServerPort(string serverIP)
+        {
+            ipString = serverIP;
+            log("Overwrote server IP = {0}", ipString);
+        }
+        public void ReportEvent(IEvent _event)
+        {
+            if (isOnline)
+            {
+                PacketPriority priority = PacketPriority.HIGH_PRIORITY;
+                PacketReliability reliability = PacketReliability.RELIABLE_ORDERED;
+                byte orderingChannel = 0;
+                uint shiftTimestamp = 0;
+
+                if (false) log("sending event: [{0}] ...", _event.ToString());
+
+                bool success = rakClientInterface.RPC(
+                    "sendeventtoserver", _event.Stream,
+                    priority, reliability, orderingChannel,
+                    RakNetBindings.UNASSIGNED_SYSTEM_ADDRESS, true,
+                    shiftTimestamp, RakNetBindings.UNASSIGNED_NETWORK_ID, null);
+
+                if (!success)
+                    log("Unable to send event to server.");
+                else
+                    log("Event was sent.");
+            }
+            else
+            {
+                log("not connected");
+            }
+        }
+        public void ProcessEvent(IEvent _event)
+        {
+            if (isOnline)
+            {
+                if (isConnected)
+                    _event.Perform();
+                else
+                {
+                    if (_event.PerformBeforeConnectOnClient())
+                        _event.Perform();
+                }
+            }
+        }
+        public bool ConnectPlayer()
+        {
+            if (isOnline)
+                return ConnectPlayer(ipString, serverPort);
+            else
+                return false;
+        }
+        public bool ConnectPlayer(string ip)
+        {
+            if (isOnline)
+                return ConnectPlayer(ip, serverPort);
+            else
+                return false;
+        }
+        public bool ConnectPlayer(string ip, ushort serverPort)
+        {
+            if (isOnline)
+            {
+                log("connecting to the server...");
+
+                int internalSleep = threadSleepTimierMS;
+                SocketDescriptor socketDescriptor = new SocketDescriptor(clientPort, string.Empty);
+
+                log("starting client on port {0}", clientPort);
+                log("connecting to server on ip= {0}, port = {1}", ip, serverPort);
+
+                rakClientInterface.Startup(1, internalSleep, new SocketDescriptor[] { socketDescriptor }, 1);
+                bool success = rakClientInterface.Connect(ip, serverPort, string.Empty, 0);
+
+                if (success)
+                {
+                    log("connected!");
+                    isConnected = true;
+                    return true;
+                }
+                else
+                {
+                    isOnline = false;
+                    isConnected = false;
+                    throw new NetworkException("Couldn't connect to server!");
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void Update()
+        {
+            if (IsOnline)
+            {
+                Packet packet = rakClientInterface.Receive();
+                while (packet != null)
+                {
+                    StringBuilder message = new StringBuilder("recieved Message from player ");
+                    message.Append(packet.systemAddress.ToString());
+                    message.AppendFormat(" from ip {0}", packet.systemAddress.binaryAddress);
+                    message.AppendFormat(" from port {0}", packet.systemAddress.port);
+
+                    BitStream stream = new BitStream(packet, false);
+                    byte eventType;  // maybe
+                    stream.Read(out eventType);
+                    message.AppendFormat(", stream = [{0}]", eventType);
+
+                    if (false) log(message.ToString());
+
+                    rakClientInterface.DeallocatePacket(packet);
+                    packet = rakClientInterface.Receive();
+                }
+            }
+        }
+        public bool IsOnline
+        {
+            get { return isOnline; }
+        }
+        #region Private Members
+        void Load(string xmlFile)
+        {
+            // TODO - Use xml reader.
+            ipString = "127.0.0.1";
+            isOnline = true;
+            serverPort = 6000;
+            clientPort = 20000;
+            threadSleepTimierMS = 0;
+        }
+        void log(string message)
+        {
+            Console.WriteLine(message);
+        }
+        void log(string format, params object[] args)
+        {
+            log(string.Format(format, args));
+        }
+        RakPeerInterface rakClientInterface;
+        string ipString;
+        ushort serverPort;
+        ushort clientPort;
+        int threadSleepTimierMS;
+        bool isOnline;
+        bool isConnected;
         #endregion
     }
 
